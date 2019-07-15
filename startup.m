64f136f1a49197ad1cc91f47184b92d64213198c
unknown <fge@princeton.edu>
2019-07-15 10:38:45 -0400

    fix

--- startup.m
+++ startup.m
@@ -1,4 +1,4 @@
-addpath('/home/fge/mfiles/slepian_oscar/')
-addpath('/home/fge/mfiles/slepian_alpha/')
-addpath('/home/fge/mfiles/slepian_zero/')
+addpath('/home/fge/Documents/MATLAB/mfiles/slepian_oscar/')
+addpath('/home/fge/Documents/MATLAB/mfiles/slepian_alpha/')
+addpath('/home/fge/Documents/MATLAB/mfiles/slepian_zero/')
 addpath('C:\Users\Frank Ge\slepian_oscar')
