52a5f39fb5a0e9b8a1119ed25caab2a318ed09a9
unknown <fge@princeton.edu>
2019-07-18 17:17:47 -0400

    hello

--- hellofilter.m
+++ hellofilter.m
@@ -1,3 +1,6 @@
+clf
+clear all
+
 w1=1/4;
 w2=1;
 w3=4;
@@ -5,42 +8,61 @@ w4=16;
 Fs=100;
 t=0:1/Fs:1-1/Fs;
 n=length(t);
-%x=cos(8*pi*t);
+z=                              cos(2*pi*w3*t);
 x=cos(2*pi*w1*t)+cos(2*pi*w2*t)+cos(2*pi*w3*t)+cos(2*pi*w4*t);
 
-subplot(321)
+ah(1)=subplot(321);
 plot(t,x);
 title('Cos signal')
 xlabel('t')
 ylabel('x(t)')
-subplot(322)
+
+ah(2)=subplot(322);
+axis=0:2/n:1-2/n;
 mag=abs(fft(x));
-plot(0:2/n:1-2/n,mag(1:length(mag)/2))
-title('Fourier Transform')
+stem(axis*n,mag(1:length(mag)/2).^2)
+title('Magnitude of Fourier Transform')
 xlabel('Frequency (pi rad/samples)')
 ylabel('Magnitude')
-subplot(323)
+
+ah(3)=subplot(323);
 [B,A]=butter(2,[.05 .15],'bandpass');
 [H,W]=freqz(B,A,n/2);
-plot(0:2/n:1-2/n,abs(H),'r')
+plot(axis*n,abs(H),'r')
 title('Filter Gain Magnitude Response')
 xlabel('Frequency (pi rad/samples)')
 ylabel('Magnitude')
-subplot(324)
-plot(0:2/n:1-2/n,angle(H),'c')
+
+ah(4)=subplot(324);
+plot(axis*n,angle(H),'c')
 title('Filter Phase Response')
 xlabel('Frequency (pi rad/samples)')
 ylabel('Phase')
-subplot(325)
+
+ah(5)=subplot(325);
 xfilt=filter(B,A,x);
 plot(t,xfilt)
 title('1 Pass Filtered')
 xlabel('t')
 ylabel('x(t)')
-subplot(326)
+
+ah(6)=subplot(326);
 xfilt2=flipud(filter(B,A,flipud(xfilt(:))));
 plot(t,xfilt2)
 title('2 Pass Filtered')
 xlabel('t')
 ylabel('x(t)')
+
+axes(ah(2))
+hold on
+mag=abs(fft(xfilt2));
+stem(axis*n,mag(1:length(mag)/2).^2)
+hold off
+
+axes(ah(6))
+hold on
+plot(t,z);
+% Cosmetics
+set(ah([2 3 4]),'xtick',[w1 w2 w3 w4]*n)
+
 
