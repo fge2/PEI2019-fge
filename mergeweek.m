39b60fadb825868d1f6569acc2c4c1564f205eaf
Frank Ge <fge@princeton.edu>
2019-07-25 14:15:28 -0400

    temp

--- mergeweek.m
+++ mergeweek.m
@@ -1,13 +1,15 @@
-function varargout=mergeweek(start,weeks)
+function varargout=mergeweek(year,start,weeks)
 % []=mergeweek(start,weeks)
 
+totaltime=zeros();
+totalTa=zeros();
 for i=1:weeks
 [time,Dm,Sm,Ta,Ua,Pa,Rc,Hc]=readweek(year,start+((i-1)*7));
 totaltime=[totaltime; time];
 totalTa=[totalTa; Ta];
 end
 
-z=find(time==0);
+z=find(totaltime==0);
 z=flip(z);
 
 for j = z'
@@ -18,7 +20,7 @@ end
 totaltime=totaltime-totaltime(1);
 
 % Optional output
-varns={totaltime,Ta};
+varns={totaltime,totalTa};
 varargout=varns(1:nargout);
 
 
